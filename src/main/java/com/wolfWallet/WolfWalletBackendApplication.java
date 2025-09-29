a747955c556a6a834ee0f595b98639c0bb56dbae
schalllaurent <laurentschallfonteilles@gmail.com>
2025-09-29 19:38:21 +0200

    first commit

---
+++ src/main/java/com/wolfWallet/WolfWalletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wolfWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfWalletBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfWalletBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolfWallet/WolfWalletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfWalletBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
